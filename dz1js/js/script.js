5c47b1d283125a69909b4be09d93b21f32d76f13
ybeyka <122684432+ybeyka@users.noreply.github.com>
2023-06-11 16:17:27 +0300

    Add files via upload
    1

---
+++ dz1js/js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+let namme ;
+let admin;
+namme = 'Oleksandr';
+admin = namme ;
+console.log(admin);
+let days;
+days = 7;
+let result;
+result = days* 24*60*60;
+console.log( days + ' днів = ' + result + ' секунд');
+   
+
+    
+
+
+
